b434dc7e54c8b2a6f1de2f49db7eb793dd20882c
GovindVermagkv <sonygovind.gkv740@gmail.com>
2022-08-05 09:15:33 +0530

    calci update

--- calculator/index.js
+++ calculator/index.js
@@ -10,8 +10,10 @@ function value(event){
     let key=event.target.innerText;
 
     if(key=="A/c"){
-        show.innerText="";
+        show.innerText="0";
     }
+   
+
     else if(key=="Del"){
         show.innerText=show.innerText.slice(0,-1);
     }
@@ -19,13 +21,59 @@ function value(event){
         show.innerText=eval(show.innerText);
     }
     else if(key=="√"){
-        show.innerText=Math.sqrt(show.innerText).toFixed(3);
+        show.innerText=Math.sqrt( show.innerText);
+    }
+    else if(key=="^"){
+        show.innerText  = Math.exp(show.innerText) ;
+    }
+    else if(key=="sin"){
+        show.innerText=Math.sin( show.innerText);
+        if(show.innerText > 0.5){
+            show.innerText='1'
+        }
+        else{
+            show.innerText=Math.sin( show.innerText);
+        }
+    }
+    else if(key=="cos"){
+        show.innerText=Math.cos( show.innerText);
+    }
+    else if(key=="tan"){
+        show.innerText=Math.tan( show.innerText);
+    }
+    else if(key=="log"){
+        show.innerText=Math.log( show.innerText);
+    }
+    else if(key=="x2"){
+        show.innerText=eval(( show.innerText)**2);
+    }
+    else if(key=="1/x"){
+        show.innerText=eval(1/( show.innerText));
+    }
+    else if(key=="e"){
+        show.innerText=Math.exp( show.innerText);
+    }
+    else if(key=="rad"){
+        show.innerText=(( show.innerText)*(3.14/180));
+    }
+    else if(key=="!"){
+        show.innerText = function myfact(fact) {
+            let factorial=1;
+            for (let i = 1; i <= 5; i++) {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+        console.log(myfact(factorial))
     }
     else if(key=="π"){
-        show.innerText=3.18*(show.innerText);
+        show.innerText +=3.14;
     }
+    
+ 
 
     else{
         show.innerText +=key
     }
 };
+
